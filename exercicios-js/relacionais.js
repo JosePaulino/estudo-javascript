40ec1e5390bd81c7ee82e3dfa2ddf4eef6f7d872
José Paulino <j.paulino1@hotmail.com>
2022-06-27 21:35:53 -0300

    Add files via upload

---
+++ exercicios-js/relacionais.js
@@ -0,0 +1,19 @@
+// === (estritamente igual, tipo e valor sao igaul)
+console.log('01)', '1' == 1) // Devido a == o retorno sera true '1' aqui é String e 1 é number
+console.log('02)', '1' == 1) // Devido a === o retorno sera false '1' aqui é String e 1 é number
+console.log('03)', '3' != 3) // so levando em conta o tipo e nao o valor
+console.log('04)', '3' !== 3) // retorna true Verifica os tipo, '3' é string e 3 é number
+
+console.log('05)', '3' < 2)
+console.log('06)', '3' > 2)
+console.log('07)', '3' <= 2)
+console.log('08)', '3' >= 2)
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log('09)', d1 === d2)
+console.log('10)', d1 == d2)
+console.log('11)', d1.getTime() === d2.getTime())
+
+console.log('12)', undefined == null)
+console.log('13)', undefined === null)
